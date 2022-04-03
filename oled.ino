dbfb04fbb53cca0e211a3787cf463a41c59dacdf
Robin <56245435+IcyFeather233@users.noreply.github.com>
2022-04-03 13:27:04 +0800

    Create oled.ino

---
+++ oled.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_64X48_ER_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.66" OLED breakout board, Uno: A4=SDA, A5=SCL, 5V powered
+
+
+
+void setup(void) {
+  u8g2.begin();
+  u8g2.enableUTF8Print();   // enable UTF8 support for the Arduino print() function
+}
+
+void loop(void) {
+  u8g2.setFont(u8g2_font_unifont_t_chinese2);  // use chinese2 for all the glyphs of "你好世界"
+  u8g2.setFontDirection(0);
+  u8g2.firstPage();
+  do {
+    u8g2.setCursor(0, 15);
+    u8g2.print("esp VLC");
+    u8g2.setCursor(0, 40);
+    u8g2.print("你好！");   // Chinese "Hello World" 
+  } while ( u8g2.nextPage() );
+  delay(1000);
+}
